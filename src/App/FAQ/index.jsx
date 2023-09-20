fe9fdee7fa9416f3c700ae8e933c03549726400a
Klaudia0099 <klaudiaastefaniak@gmail.com>
2023-09-20 21:57:44 +0200

    FAQ/design

--- src/App/FAQ/index.jsx
+++ src/App/FAQ/index.jsx
@@ -1,44 +1,56 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-
-import { NotFound } from '../Components/NotFound/NotFound';
-
+import React, { useState } from 'react';
 import './styles.css';
 
-export function FAQ() {
-  return (
-    <Routes>
-      <Route path="" element={<FAQLayout />}>
-      </Route>
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  );
-}
+const faqData = [
+  {
+    question: 'What programming languages and technologies do I know?',
+    answer:
+      'I have a basic knowledge of several programming languages: HTML/CSS, JavaScript, TypeScript. Additionally, I have experience working with various tools, frameworks, and libraries such as React, GIT, Firebase, Trello.',
+  },
+  {
+    question: 'Am I open to new technologies and how quickly do I learn?',
+    answer:
+      "Yes, I'm always open to learning new technologies and try to stay up-to-date with the latest trends and tools in the IT industry. I believe that my ability to quickly acquire new skills is one of my strengths.",
+  },
+  {
+    question: 'How do I take care of the development of my skills?',
+    answer:
+      'I take care of the development of my skills by regularly participating in onsite and online courses. I also work on my projects to gain experience and learn about new technologies.',
+  },
+  {
+    question:
+      'What is my experience with team collaboration and using version control systems?',
+    answer:
+      'I have worked in teams of various sizes and structures. I can manage people, effectively communicate with other team members, and collaborate towards common goals. I also have experience using version control systems, such as Git.',
+  },
+];
 
-function FAQLayout() {
+export function FAQ() {
+  const [selectedQuestion, setSelectedQuestion] = useState(null);
+
   return (
     <div className="faq-main-container">
       <h1>FAQ</h1>
-      <h5>Tutaj znajdź odpowiedzi na najczęściej zadawane pytania</h5>
-
-      <div className="question-main-container">
-        <div className='question-arrow'>&lt;</div><span>Jak mogę zapisać się na szkolenie?</span>
-        <hr />
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eleifend rutrum ipsum, leo et in. Mattis porttitor volutpat placerat suspendisse ante in cursus etiam ullamcorper. Sollicitudin egestas aliquam, adipiscing adipiscing iaculis habitant. Viverra pretium tincidunt nisl pellentesque ut adipiscing non.Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-      </div>
-      <br />
-      <div className="question-main-container-1">
-        <div className='question-arrow'>&lt;</div><span>Jak mogę zapisać się na szkolenie?</span>
-      </div>
-      <br />
-      <div className="question-main-container-1">
-        <div className='question-arrow'>&lt;</div><span>Jak mogę zapisać się na szkolenie?</span>
-      </div>
-      <br />
-      <div className="question-main-container-1">
-        <div className='question-arrow'>&lt;</div><span>Jak mogę zapisać się na szkolenie?</span>
+      <h5>
+        Here you will find answers to the most frequently asked questions.
+      </h5>
+      <div className="border-box">
+        {faqData.map((item, index) => (
+          <div key={index} className="faq-item">
+            <h3
+              className="question"
+              onClick={() =>
+                setSelectedQuestion(selectedQuestion === index ? null : index)
+              }
+            >
+              {item.question}
+            </h3>
+            {selectedQuestion === index && (
+              <p className="answer">{item.answer}</p>
+            )}
+          </div>
+        ))}
       </div>
     </div>
-
   );
 }
